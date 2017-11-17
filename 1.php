db6fc3dbd5de9f04d9ecded9137f342813d9c7a7
sunhaoran <1208818417@qq.com>
2017-11-17 17:55:23 +0800

    tcp

--- 1.php
+++ 1.php
@@ -1,6 +1,17 @@
 <?php
 
-$server = new swoole_server('127.0.0.1',9999);
+$server = new swoole_server('192.168.23.92',9999);
 $server->set(array(
+	'worker_num' => 2,    //开启两个worker进程
+	'max_request' => 3,   //每个worker进程max request设置为3次
+	'dispatch_mode'=>3,
+));
+$server->on('receive', function ($server, $fd, $from_id, $data) {
+    //$server->send($fd, "Server: ".$data);
+    	foreach($server->connections as $fd1)
+	{
+	    $server->send($fd1, $fd." say:".$data.PHP_EOL);
+	}
+});
 
-    ));
+$server->start();
